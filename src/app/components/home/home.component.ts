0addfa1e7621770269ba662b167c470834f55b2f
Stefhanny Lazaro <stefhannylr19@gmail.com>
2020-03-03 20:35:11 -0500

    host scroll

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+@HostListener('scroll', ['$event'])
 export class HomeComponent implements OnInit {
 
   constructor() { }
@@ -12,5 +13,11 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     console.log("Hola home");
   }
-
+  onScroll(event: any) {
+    // visible height + pixel scrolled >= total height 
+    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
+      console.log("Parte final del elem.");
+    }
+    console.log(event.target.scrollTop);
+  }
 }
